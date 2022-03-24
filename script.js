ba9f909669a1b8d5d99121501660b95c27f62b87
exmami <76182462+exmami@users.noreply.github.com>
2022-03-24 08:02:27 -0600

    computation, colors and the alignment of the form elements

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
+function updateRate()
+{
+    // update changes of slider
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerHTML=rateval+"%";
+}
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+
+    var rate = document.getElementById("rate").value;
+
+    var years = document.getElementById("years").value;
+
+    // an alert If principal amount field is blank, is zero or is a negative number
+    if  (principal <= "0"){
+        alert("Enter a positive number");
+        principal.focus(); // focus to the Principal input box
+        // return false;
+    }
+    else{
+        // calculating interest
+        var interest = principal * rate * years/100;
+        // current date + var years
+        var year = new Date().getFullYear()+parseInt(years);
+        // put the result inside the html and display
+        document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark></br>"+
+        "at an interest rate of <mark>"+rate+"%</mark> <br>"+
+        "You will receive an amount of <mark>"+interest+"</mark><br>"+
+        "in the year <mark>"+year+"</mark>" // highlight with the tag mark
+    }
 }
-        
